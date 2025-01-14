c3c6befd38e7e4f0abd8c9eb602c1a19e2bf6fd5
kimsu10 <betnwjd25@gmail.com>
2025-01-14 13:49:36 +0900

    UPDATE: footer

--- src/Router.js
+++ src/Router.js
@@ -1,3 +1,179 @@
+// import React, { useEffect, useState } from "react";
+// import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+// import Main from "./pages/Main/Main";
+// import Footer from "./components/Footer/Footer";
+// import Nav from "./components/Nav/Nav";
+// import Practice from "./pages/Practice/Practice";
+// import FinishPage from "./pages/Practice/FinishPage";
+// import NotFound from "./pages/NotFound/NotFound";
+// import NcpMain from "./pages/Main/NcpMain";
+// import Exam from "./pages/Exam/Exam";
+// import ExamFinishPage from "./pages/Practice/ExamFinishPage";
+// import NcaMain from "./pages/Main/NcaMain";
+// import TestMatch from "./pages/TestMatch/TestMatch";
+// import MatchResult from "./pages/TestMatch/MatchResult";
+// import MatchWaiting from "./pages/TestMatch/MatchWaiting";
+// import Quiz from "./pages/TestMatch/Quiz";
+// import FindUserPage from "./pages/User/FindUserPage";
+// import Admin from "./pages/Admin/Admin";
+// import Sponsor from "./components/Ads/Sponsor";
+// import SponsorSuccess from "./components/Ads/SponsorSuccess";
+// import SponsorFail from "./components/Ads/SponsorFail";
+// import Bookmarks from "./pages/User/Bookmarks";
+
+// const Router = () => {
+//   const [username, setUsername] = useState("");
+//   const token = sessionStorage.getItem("accessToken");
+//   const [isAdmin, setIsAdmin] = useState(false);
+
+//   useEffect(() => {
+//     if (token) {
+//       const payload = token.split(".")[1];
+//       const base64Url = payload.replace(/-/g, "+").replace(/_/g, "/");
+//       const decodedPayload = atob(base64Url);
+//       const decodedText = new TextDecoder("utf-8").decode(
+//         Uint8Array.from(decodedPayload, c => c.charCodeAt(0)),
+//       );
+//       const decodedUsername = decodeURIComponent(decodedText);
+//       const parsedPayload = JSON.parse(decodedUsername);
+//       setUsername(parsedPayload.sub);
+
+//       // const decodedPayload = JSON.parse(atob(payload));
+//       // setUsername(decodedPayload.sub);
+
+//       // 관리자 여부 확인
+//       const role = decodedPayload.auth;
+//       if (role === "ROLE_ADMIN") {
+//         setIsAdmin(true);
+//       }
+//     }
+//   }, [token]);
+
+//   return (
+//     <BrowserRouter>
+//       {isAdmin ? (
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <>
+//                 <Nav username={username} />
+//                 <Admin />
+//               </>
+//             }
+//           />
+//         </Routes>
+//       ) : (
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               <>
+//                 <Nav nick={username} />
+//                 <Main />
+//               </>
+//             }
+//           />
+//           <Route path="/:name" element={<PageSwitch username={username} />} />
+//           <Route
+//             path="/:name/practice"
+//             element={<PageWrapper username={username} Component={Practice} />}
+//           />
+//           <Route
+//             path="/:name/exam"
+//             element={<PageWrapper username={username} Component={Exam} />}
+//           />
+//           <Route
+//             path="/:name/practice/finish"
+//             element={<PageWrapper username={username} Component={FinishPage} />}
+//           />
+//           <Route
+//             path="/:name/exam/finish"
+//             element={
+//               <PageWrapper username={username} Component={ExamFinishPage} />
+//             }
+//           />
+//           <Route
+//             path="/:name/who-are-you"
+//             element={<PageWrapper username={username} Component={NotFound} />}
+//           />
+//           <Route
+//             path="/bookmarks/:subjectName"
+//             element={<PageWrapper Component={Bookmarks} />}
+//           />
+//           <Route
+//             path="/quiz"
+//             element={<PageWrapper username={username} Component={Quiz} />}
+//           />
+//           <Route
+//             path="/quiz/:selectedName/:roomName"
+//             element={
+//               <PageWrapper username={username} Component={MatchWaiting} />
+//             }
+//           />
+//           <Route
+//             path="/quiz/:selectedName/:roomName/result"
+//             element={
+//               <PageWrapper username={username} Component={MatchResult} />
+//             }
+//           />
+//           <Route
+//             path="/find-account"
+//             element={<PageWrapper Component={FindUserPage} />}
+//           />
+//           {/* 이 아래는 후원기능용 테스트 겸 사이즈 확인용  */}
+//           <Route
+//             path="/sponsor"
+//             username={username}
+//             element={<PageWrapper Component={Sponsor} />}
+//           />
+//           <Route
+//             path="/sponsor/success"
+//             element={<PageWrapper Component={SponsorSuccess} />}
+//           />
+//           <Route
+//             path="/sponsor/fail"
+//             element={<PageWrapper Component={SponsorFail} />}
+//           />
+//         </Routes>
+//       )}
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// };
+
+// const PageSwitch = ({ username }) => {
+//   const { name } = useParams();
+
+//   const getComponent = () => {
+//     switch (name) {
+//       case "NCA":
+//         return <NcaMain />;
+//       default:
+//         return <NcpMain />;
+//     }
+//   };
+
+//   return (
+//     <>
+//       <Nav username={username} subjectName={name} />
+//       {getComponent()}
+//     </>
+//   );
+// };
+
+// const PageWrapper = ({ username, Component }) => {
+//   const { name, selectedName } = useParams();
+//   return (
+//     <>
+//       <Nav username={username} subjectName={name || selectedName} />
+//       <Component username={username} />
+//     </>
+//   );
+// };
+
+// export default Router;
+
 import React, { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 import Main from "./pages/Main/Main";
@@ -38,10 +214,6 @@ const Router = () => {
       const parsedPayload = JSON.parse(decodedUsername);
       setUsername(parsedPayload.sub);
 
-      // const decodedPayload = JSON.parse(atob(payload));
-      // setUsername(decodedPayload.sub);
-
-      // 관리자 여부 확인
       const role = decodedPayload.auth;
       if (role === "ROLE_ADMIN") {
         setIsAdmin(true);
@@ -51,93 +223,159 @@ const Router = () => {
 
   return (
     <BrowserRouter>
-      {isAdmin ? (
-        <Routes>
+      <Routes>
+        {isAdmin ? (
           <Route
             path="/"
             element={
               <>
                 <Nav username={username} />
                 <Admin />
+                <Footer />
               </>
             }
           />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Nav nick={username} />
-                <Main />
-              </>
-            }
-          />
-          <Route path="/:name" element={<PageSwitch username={username} />} />
-          <Route
-            path="/:name/practice"
-            element={<PageWrapper username={username} Component={Practice} />}
-          />
-          <Route
-            path="/:name/exam"
-            element={<PageWrapper username={username} Component={Exam} />}
-          />
-          <Route
-            path="/:name/practice/finish"
-            element={<PageWrapper username={username} Component={FinishPage} />}
-          />
-          <Route
-            path="/:name/exam/finish"
-            element={
-              <PageWrapper username={username} Component={ExamFinishPage} />
-            }
-          />
-          <Route
-            path="/:name/who-are-you"
-            element={<PageWrapper username={username} Component={NotFound} />}
-          />
-          <Route
-            path="/:username/bookmarks"
-            element={<PageWrapper Component={Bookmarks} />}
-          />
-          <Route
-            path="/quiz"
-            element={<PageWrapper username={username} Component={Quiz} />}
-          />
-          <Route
-            path="/quiz/:selectedName/:roomName"
-            element={
-              <PageWrapper username={username} Component={MatchWaiting} />
-            }
-          />
-          <Route
-            path="/quiz/:selectedName/:roomName/result"
-            element={
-              <PageWrapper username={username} Component={MatchResult} />
-            }
-          />
-          <Route
-            path="/find-account"
-            element={<PageWrapper Component={FindUserPage} />}
-          />
-          {/* 이 아래는 후원기능용 테스트 겸 사이즈 확인용  */}
-          <Route
-            path="/sponsor"
-            username={username}
-            element={<PageWrapper Component={Sponsor} />}
-          />
-          <Route
-            path="/sponsor/success"
-            element={<PageWrapper Component={SponsorSuccess} />}
-          />
-          <Route
-            path="/sponsor/fail"
-            element={<PageWrapper Component={SponsorFail} />}
-          />
-        </Routes>
-      )}
-      <Footer />
+        ) : (
+          <>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Nav nick={username} />
+                  <Main />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/:name"
+              element={
+                <>
+                  <PageSwitch username={username} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/:name/practice"
+              element={
+                <>
+                  <PageWrapper username={username} Component={Practice} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/:name/exam"
+              element={
+                <>
+                  <PageWrapper username={username} Component={Exam} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/:name/practice/finish"
+              element={
+                <>
+                  <PageWrapper username={username} Component={FinishPage} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/:name/exam/finish"
+              element={
+                <>
+                  <PageWrapper username={username} Component={ExamFinishPage} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/:name/who-are-you"
+              element={
+                <>
+                  <PageWrapper username={username} Component={NotFound} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/bookmarks/:subjectName"
+              element={
+                <>
+                  <PageWrapper Component={Bookmarks} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/quiz"
+              element={
+                <>
+                  <PageWrapper username={username} Component={Quiz} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/quiz/:selectedName/:roomName"
+              element={
+                <>
+                  <PageWrapper username={username} Component={MatchWaiting} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/quiz/:selectedName/:roomName/result"
+              element={
+                <>
+                  <PageWrapper username={username} Component={MatchResult} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/find-account"
+              element={
+                <>
+                  <PageWrapper Component={FindUserPage} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/sponsor"
+              element={
+                <>
+                  <PageWrapper Component={Sponsor} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/sponsor/success"
+              element={
+                <>
+                  <PageWrapper Component={SponsorSuccess} />
+                  <Footer />
+                </>
+              }
+            />
+            <Route
+              path="/sponsor/fail"
+              element={
+                <>
+                  <PageWrapper Component={SponsorFail} />
+                  <Footer />
+                </>
+              }
+            />
+          </>
+        )}
+      </Routes>
     </BrowserRouter>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,138 @@
+// import { Link, useNavigate, useLocation } from "react-router-dom";
+// import styled from "styled-components";
+
+// const Footer = () => {
+//   const navigate = useNavigate();
+//   const location = useLocation();
+
+//   const isSponsorPage = location.pathname.startsWith("/sponsor");
+//   const isMainPage = location.pathname === "/";
+
+//   return (
+//     <FooterBody isMainPage={isMainPage}>
+//       <div className="introduce-box">
+//         <p className="introduce-text">
+//           이 사이트는 네이버 클라우드 공식 웹사이트와 관련이 없습니다.
+//           <br />
+//           네이버 클라우드 시험 접수 및 문의는 &nbsp;
+//           <Link to="https://edu.ncloud.com/certi">
+//             <span className="go-to-navercloud">네이버 클라우드 공식페이지</span>
+//           </Link>
+//           에서 가능합니다.
+//         </p>
+//         <a
+//           href="mailto:neta@gmail.com"
+//           className="send-inquiry"
+//           style={{ color: "gray" }}
+//         >
+//           개발자에게 문의하기
+//         </a>
+//       </div>
+//       <div className="button-box">
+//         {!isSponsorPage && (
+//           <>
+//             <button
+//               className="sponsor-button"
+//               onClick={() => alert("현재 테스트 단계입니다.")}
+//             >
+//               ☕️&nbsp;후원하기
+//             </button>
+//             <button
+//               className="mini-sponsor-button"
+//               onClick={() => alert("현재 테스트 단계입니다.")}
+//             >
+//               ☕️
+//             </button>
+//           </>
+//         )}
+//       </div>
+//     </FooterBody>
+//   );
+// };
+
+// export default Footer;
+
+// const FooterBody = styled.div`
+//   width: 100%;
+//   height: auto;
+//   padding: 1rem;
+//   background-color: #f3f4f8;
+//   text-align: center;
+//   z-index: 10;
+//   padding: 2.4rem;
+//   display: flex;
+//   justify-content: space-between;
+//   position: ${props => (props.isMainPage ? "fixed" : "relative")};
+//   bottom: ${props => (props.isMainPage ? "0" : "auto")};
+//   left: ${props => (props.isMainPage ? "0" : "auto")};
+//   margin-top: auto;
+
+//   .introduce-box {
+//     text-align: start;
+//     color: gray;
+
+//     .introduce-text {
+//       margin-bottom: 1rem;
+
+//       .go-to-navercloud {
+//         cursor: pointer;
+//         font-weight: 600;
+//         color: gray;
+
+//         &:hover {
+//           text-decoration: underline;
+//         }
+//       }
+//     }
+
+//     .send-inquiry {
+//       cursor: pointer;
+//       font-weight: 600;
+
+//       &:hover {
+//         text-decoration: underline;
+//       }
+//     }
+//   }
+
+//   .button-box {
+//     position: relative;
+
+//     .sponsor-button {
+//       width: 10rem;
+//       height: 3.4rem;
+//       background-color: black;
+//       font-size: 1.1rem;
+//       display: block;
+//     }
+
+//     .mini-sponsor-button {
+//       line-height: 0.8;
+//       background-color: black;
+//       border-radius: 50%;
+//       width: 3rem;
+//       height: 3rem;
+//       font-size: 1.2rem;
+//       position: absolute;
+//       top: -7rem;
+//       right: 0;
+//       display: none;
+//     }
+//   }
+
+//   @media (max-width: 780px) {
+//     .button-box {
+//       .sponsor-button {
+//         display: none;
+//       }
+
+//       .mini-sponsor-button {
+//         display: block;
+//       }
+//     }
+//   }
+// `;
+
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
@@ -5,10 +140,12 @@ const Footer = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const isSponsorPage = location.pathname.startsWith("/sponsor");
+  // 경로가 "/" 또는 "/NCA"인 경우 스타일을 고정하도록 설정
+  const isFixedStyle =
+    location.pathname === "/" || location.pathname === "/NCA";
 
   return (
-    <FooterBody>
+    <FooterBody isFixedStyle={isFixedStyle}>
       <div className="introduce-box">
         <p className="introduce-text">
           이 사이트는 네이버 클라우드 공식 웹사이트와 관련이 없습니다.
@@ -28,18 +165,16 @@ const Footer = () => {
         </a>
       </div>
       <div className="button-box">
-        {!isSponsorPage && (
+        {!location.pathname.startsWith("/sponsor") && (
           <>
             <button
               className="sponsor-button"
-              // onClick={() => navigate("/sponsor")}
               onClick={() => alert("현재 테스트 단계입니다.")}
             >
               ☕️&nbsp;후원하기
             </button>
             <button
               className="mini-sponsor-button"
-              // onClick={() => navigate("/sponsor")}
               onClick={() => alert("현재 테스트 단계입니다.")}
             >
               ☕️
@@ -50,11 +185,11 @@ const Footer = () => {
     </FooterBody>
   );
 };
+
 export default Footer;
 
 const FooterBody = styled.div`
   width: 100%;
-  /* min-width: 28rem; */
   height: auto;
   padding: 1rem;
   background-color: #f3f4f8;
@@ -63,8 +198,10 @@ const FooterBody = styled.div`
   padding: 2.4rem;
   display: flex;
   justify-content: space-between;
-  bottom: 0;
-  position: fixed;
+  position: ${props => (props.isFixedStyle ? "fixed" : "relative")};
+  bottom: ${props => (props.isFixedStyle ? "0" : "auto")};
+  left: ${props => (props.isFixedStyle ? "0" : "auto")};
+  margin-top: auto;
 
   .introduce-box {
     text-align: start;
